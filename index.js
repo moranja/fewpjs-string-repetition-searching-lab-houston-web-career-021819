8aa4fb8cadda777a461d1e3c0f07413ffcb6951a
moranja <jeffadammoran@gmail.com>
2019-04-11 15:25:17 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution here
+let heyJude = function (verse) {
+  return verse.repeat(16)
+}
+
+let prohibitedLanguage = function(potentialName) {
+  return potentialName.search(/candycorn|brusselssprouts/)
+}
